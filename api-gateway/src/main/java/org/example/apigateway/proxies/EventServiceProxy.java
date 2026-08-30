9a2e129b8116793fe78eb8e22f40bc5c858c1068
amit.bhagra <amit.bhagra@gmail.com>
2026-08-30 19:12:09 +0530

    Added config files

--- api-gateway/src/main/java/org/example/apigateway/proxies/EventServiceProxy.java
+++ api-gateway/src/main/java/org/example/apigateway/proxies/EventServiceProxy.java
@@ -1,6 +1,7 @@
 package org.example.apigateway.proxies;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import org.springframework.web.reactive.function.client.WebClient;
@@ -10,13 +11,15 @@ import reactor.core.publisher.Mono;
 public class EventServiceProxy {
 
   private final WebClient client;
+  private final String eventServiceBaseUrl;
 
-  public EventServiceProxy() {
+  public EventServiceProxy(@Value("${EVENT_SERVICE_URL:http://localhost:8092}") String eventServiceBaseUrl) {
     this.client = WebClient.create();
+    this.eventServiceBaseUrl = eventServiceBaseUrl;
   }
 
   public Mono<EventInfo> findEventById(String eventId) {
-    String url = "http://localhost:8092/api/v1/events/" + eventId;
+    String url = eventServiceBaseUrl + "/api/v1/events/" + eventId;
     return client.get()
             .uri(url)
             .retrieve()
@@ -24,7 +27,7 @@ public class EventServiceProxy {
   }
 
   public Mono<EventInfo> findEventById(String eventId, String authHeader) {
-    String url = "http://localhost:8092/api/v1/events/" + eventId;
+    String url = eventServiceBaseUrl + "/api/v1/events/" + eventId;
     return client.get()
             .uri(url)
             .header("Authorization", authHeader)
